06b913ce0cd4d5b0bb6bbaf4878718166864df62
Daniel <drborsato@gmail.com>
2017-08-27 20:57:32 -0300

    add arquivos

---
+++ src/main/java/course/week01/classes/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,25 @@
+package course.week01.classes;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HelloWorldFreeMarkerStyle {
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(HelloWorldFreeMarkerStyle.class, "/");
+		try {
+			Template helloTemplate = configuration.getTemplate("hello.ftl");
+			StringWriter writer = new StringWriter();;
+			Map<String,Object> helloMap = new HashMap<String, Object>();
+			helloMap.put("name", "FreeMarker");
+			helloTemplate.process(helloMap, writer);
+			System.out.println(writer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
